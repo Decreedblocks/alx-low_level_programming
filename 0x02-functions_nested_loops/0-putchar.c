9fa0df88a355e489b01620d45d9fc98181cd8530
Decreedblocks <emekky54@gmail.com>
2022-09-13 05:39:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
-* main - Entry point
-* Return:0
+* main - prints Putchar
+*
+* Description: prints _putchar
+*
+* Return: 0 (Success)
 **/
 int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
